f788e4fa5d967537d70c6f3431a9b78914b946c5
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-05-06 15:38:26 +0200

    Updates

---
+++ precache-manifest.14a5ed03130351fb203678e77e213b7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1671a28d08467026320cd81038a5099",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "ae0b8c0da59de1abbd83",
+    "url": "/hangman/static/css/main.d6e43c87.chunk.css"
+  },
+  {
+    "revision": "bedea4066ca0672b7b81",
+    "url": "/hangman/static/js/2.83b9a475.chunk.js"
+  },
+  {
+    "revision": "ae0b8c0da59de1abbd83",
+    "url": "/hangman/static/js/main.90aaccd1.chunk.js"
+  },
+  {
+    "revision": "19ffbe7598942725a8e1",
+    "url": "/hangman/static/js/runtime~main.d9fce119.js"
+  },
+  {
+    "revision": "6c5eea1bc6934baf358b1e367475fae9",
+    "url": "/hangman/static/media/hangman.6c5eea1b.gif"
+  }
+]);
